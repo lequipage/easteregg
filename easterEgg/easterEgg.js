19ef13d91347a889a6188ef8c538b11667ca6496
BuildTools <unconfigured@null.spigotmc.org>
2017-12-08 00:44:07 +0100

    Ajout de l'Easter Egg "Drunk Effect"

--- easterEgg/easterEgg.js
+++ easterEgg/easterEgg.js
@@ -1,12 +1,40 @@
 function easterEgg(){
 }
 
-easterEgg.prototype.caca = function(){
-  console.log("Tata");
+var current_tip = "";
+
+// Tip Something
+easterEgg.prototype.tipSomething = function(e, el, word, action){
+  if ($('input:focus').length == 0){
+    current_tip += String.fromCharCode(e.which);
+    console.log(current_tip);
+    if(current_tip.includes(word)){
+        if (action == "drunk")
+          this.drunk(el);
+        current_tip = "";
+    }
+  }
 }
 
-easterEgg.prototype.drunk = function(source = 0, timer = 0, force = 1) {
-  source = force + 2;
-  console.log(source);
-  this.caca();
+/*Function Drunk*/
+easterEgg.prototype.drunk = function (el, it = 3, t = 4, f = 1) {
+    let c = 0;
+    const a = () => {
+        if (c < it) {
+            el.setAttribute("style", "transition: "+t+"s; filter: blur(" + f + "px); transform: scale(1.02); transform: rotate(0.4deg); overflow: hidden;");
+            setTimeout(function () {
+            el.setAttribute("style", "transition: "+t+"s; filter: blur(0px); transform: scale(1); transform: rotate(-0.4deg); overflow: hidden;");
+            setTimeout(function () {
+                a();
+            }, t * 1000 / 2);
+        }, t * 1000 / 2);
+            c++;
+        } else {
+            el.setAttribute("style", "transition: "+t+"s;");
+            setTimeout(function () {
+                el.setAttribute("style", "");
+            }, t * 1000 / 2);
+        }
+    }
+    a();
 }
